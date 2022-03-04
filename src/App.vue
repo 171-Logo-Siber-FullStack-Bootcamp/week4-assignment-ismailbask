67764335a250fa6f5eeeaf963a850ff2065c38a3
İsmail Basık <87307720+ismailbask@users.noreply.github.com>
2022-03-04 18:13:41 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <SideBar/>  
+  <TopBar/>
+  <MainTemplete/>
+  
+</template>
+
+<script>
+import MainTemplete from './components/MainTemplete.vue'
+import SideBar from './components/SideBar.vue'
+import TopBar from './components/TopBar'
+
+
+
+
+
+export default {
+  name: 'App',
+  components: {
+    MainTemplete,
+    SideBar,
+    TopBar,
+
+
+  }
+}
+</script>
+
+<style>
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+}
+</style>
